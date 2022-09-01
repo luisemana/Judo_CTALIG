b79d1ed173a59bece07b9b749880074af541029f
luisemana <luis_semana@hotmail.com>
2022-09-01 10:32:47 -0300

    importei tabela

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+
 
 st.title('Aplicativo')
+
+df = pd.DataFrame(
+    np.random.randn(10, 5),
+    columns=('col %d' % i for i in range(5)))
+
+st.table(df)
